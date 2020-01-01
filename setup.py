1a5980c3cf4b03a5a0430d342a0bc72401943980
PiecePaperCode <marcos.gam@hotmail.com>
2020-01-01 22:29:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,1 @@
-from setuptools import setup
-
-setup(
-   name='pyogame2',
-   version='7.1.0',
-   description='This lib is supposed to help write scripts and bots for your needs. it supports ogame_version: 7.1.0',
-   author='PiecePaperCode',
-   packages=['pyogame2'],  #same as name
-   install_requires=[]
-)
+packages=['pyogame'],
