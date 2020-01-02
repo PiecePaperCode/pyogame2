8dc9f863e9c083dc57239775b3d44bd83ecfa3ff
PiecePaperCode <40020346+PiecePaperCode@users.noreply.github.com>
2020-01-02 03:36:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ setup(
   license='MIT',
   description = 'lib for the popular browsergame ogame',
   author = 'PapeprPieceCode',
-  author_email = 'marcos.gam@hotmail.com',      # Type in your E-Mail
-  url = 'https://github.com/PiecePaperCode/pyogame2',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/PiecePaperCode/pyogame2.git',    # I explain this later on
-  keywords = ['OGame', 'lib', 'for bots'],   # Keywords that define your package best
+  author_email = 'marcos.gam@hotmail.com',      
+  url = 'https://github.com/PiecePaperCode/pyogame2',   
+  download_url = 'https://github.com/PiecePaperCode/pyogame2.git',    
+  keywords = ['OGame', 'lib', 'for bots'],   
   install_requires=[],
   classifiers=[
-    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.6',
   ],
 )
